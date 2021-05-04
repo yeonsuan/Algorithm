5a4413f7e32bdb11b08b73853ebf32b6a49adf04
yeonsuan <sweetsuan@naver.com>
2021-05-05 02:38:17 +0900

    upload: 짝수판별 알고리즘 업로드

---
+++ evenodd.js
@@ -0,0 +1,12 @@
+function solution(num) {
+    var answer;
+    if (num % 2 ===0){answer = 'Even'}
+    else {answer = 'Odd'};
+    return answer;
+}
+
+
+
+function evenOrOdd(num){
+   return num % 2? "Odd" : "Even";
+}
