1e45c788cddf9a12e3bd327b0d6897133284a110
yeonsuan <sweetsuan@naver.com>
2021-07-01 10:49:50 +0900

    알고리즘 section3

--- section3/e_pushword.js
+++ section3/e_pushword.js
@@ -1,6 +1,6 @@
 function solution(s) {
-    let answer='';                              //answer에 채워넣어야하니까
-    let cnt = 1;                                //H나 E는 1개씩 있으니까, 0으로하면안됨!
+    let answer='';       //answer에 채워넣어야하니까
+    let cnt = 1;         //H나 E는 1개씩 있으니까, 0으로하면안됨!
     s=s+' ';                                    //마지막 E도 처리해야하니까 빈칸하나 더 만들어놔야해.
     for(let i =0; i<s.length-1; i++){
         if(s[i]===s[i+1]){
