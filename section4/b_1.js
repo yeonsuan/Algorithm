b5f18ce8a15797f597cab29fdccc86b3241acd62
yeonsuan <sweetsuan@naver.com>
2021-07-04 01:39:29 +0900

    Add solve

--- section4/b_1.js
+++ section4/b_1.js
@@ -1,28 +1,50 @@
 //뒤집은 소수
-function solution(n,arr){
-    let answer='';
+function isPrime(num){
+    if(num===1) return false;
+    for(let i=2; i<=parseInt(Math.sqrt(num)); i++){
+        if(num%i===0) return false;
+    }
+    return true;
+}
+function solution(arr){
+    let answer=[];
     for(let x of arr){
-        if(x.split('').reverse.joint('')){ //여기서 이제 바뀜
-                if (x <= 1) return false;
-                if (x === 2 || x === 3) answer+='x';
-                if (x % 2 === 0) return false;
-
-            let divisor = 3;
-            while (x > divisor) {
-            
-                if (x % divisor === 0) return false;
-
-             
-                divisor += 2;
-            }
-
-          
-            answer+='x'
+        let res=0;
+        while(x){
+            let t=x%10;
+            res=res*10+t;
+            x=parseInt(x/10);
         }
-
-        }
+        if(isPrime(res)) answer.push(res);
+    }
     return answer;
 }
 
-let arr = [32,55,62,20,250,370,200,30,100];
-console.log(solution(9,arr));
+let arr=[32, 55, 62, 20, 250, 370, 200, 30, 100];
+console.log(solution(arr));
+
+
+
+
+
+// function solution(n,arr){
+//     let answer='';
+//     for(let x of arr){
+//         x.split('').reverse.joint('')//여기서 이제 바뀜
+//         if(x<=1)return false;
+//         if (x===2 || x===3) answer+=x;
+//         if(n%2)return false;
+//         let divisor=3;
+//         while(x>divisor){
+//             if(n%divisor===0)return false;
+
+//             divisor+=2;
+//         }
+
+//         }
+
+//     return answer;
+// }
+
+// let arr = [32,55,62,20,250,370,200,30,100];
+// console.log(solution(9,arr));
