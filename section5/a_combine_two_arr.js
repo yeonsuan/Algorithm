6f71ffe1d92ceef927ae42d523e8ffccd42c5bb2
yeonsuan <sweetsuan@naver.com>
2021-07-08 15:49:58 +0900

    combine_two_arr

---
+++ section5/a_combine_two_arr.js
@@ -0,0 +1,15 @@
+//두 배열 합치기 (0708)
+function solution(arr,arr2){
+    let answer;
+    let newArr=[
+        ...arr,
+        ...arr2
+    ];
+    newArr.sort((a,b)=>a-b);
+    answer=newArr;
+    return answer;
+}
+
+let arr = [1,3,5];
+let arr2 = [2,3,6,7,9];
+console.log(solution(arr,arr2));
