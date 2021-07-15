ecd37e46fff73ebeca455e831cc291f2c8406ed8
yeonsuan <sweetsuan@naver.com>
2021-07-15 16:26:06 +0900

    내일 다시 고민하기!!오늘은 끝

---
+++ Inflearn/section5/f_chairman_alone.js
@@ -0,0 +1,19 @@
+//학급 회장/ 해시란? 임의의 크기를 가진 데이터를 고정된 데이터의 크기로 변환시키는 것
+//내일 다시 풀어보기~~~~!!
+
+const solution = (arr) => {
+    let answer;
+    arr.split(""); //배열로 만든 후 거기서 제일 많은 문자를 구하기!!!(이건 해시아님)
+    for(let x of arr){
+        if(arr[x]){
+            arr[x]++;
+        }else {
+            arr[x] = 1;
+        }
+    }
+    answer=arr[x];
+    return answer;
+}
+
+let a = "BACBACCACCBDEDE";
+console.log(solution(a));
